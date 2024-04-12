b3c45f9f5c7681c6098984f75d9333c5fa88ea41
ThePanaJofran <123037327+ThePanaJofran@users.noreply.github.com>
2024-04-11 21:28:41 -0500

    Add files via upload

---
+++ src/lol/reto/LolReto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lol.reto;
+
+/**
+ *
+ * @author Lester
+ */
+public class LolReto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
